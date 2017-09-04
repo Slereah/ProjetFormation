5545c970df10f11caf745fae40eded9d2b5f0ba9
MariusDelannoy <marius.dial@msn.com>
2017-09-04 11:17:27 +0200

    user profile

--- app/Views/navigation.php
+++ app/Views/navigation.php
@@ -1,6 +1,8 @@
 <nav>
 
-	<a href="<?= $this->url("security_signup") ?>">Inscription</a>
-	<a href="<?= $this->url("security_signin") ?>">Identification</a>
+	<a href="<?= $this->url("home") ?>">Acceuil</a>  |
+	<a href="<?= $this->url("security_signup") ?>">Inscription</a>	|
+	<a href="<?= $this->url("security_signin") ?>">Identification</a>	|
+	<a href="<?= $this->url("profile") ?>">Profile</a>
 
 </nav>
