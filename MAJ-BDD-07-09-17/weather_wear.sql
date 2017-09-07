74db24f4fd5fff8550255eb84558ba0f37f02696
Delphine <d-martinet@outlook.fr>
2017-09-07 09:58:11 +0200

    Ajout table contact dans BDD

--- MAJ-BDD-07-09-17/weather_wear.sql
+++ MAJ-BDD-07-09-17/weather_wear.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Client :  127.0.0.1
--- Généré le :  Mer 06 Septembre 2017 à 11:32
+-- Généré le :  Jeu 07 Septembre 2017 à 09:45
 -- Version du serveur :  10.1.21-MariaDB
 -- Version de PHP :  5.6.30
 
@@ -40,6 +40,22 @@ CREATE TABLE `clothes` (
 -- --------------------------------------------------------
 
 --
+-- Structure de la table `contact`
+--
+
+CREATE TABLE `contact` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `firstname` varchar(40) NOT NULL,
+  `lastname` varchar(40) NOT NULL,
+  `email` varchar(120) NOT NULL,
+  `message` text NOT NULL,
+  `date` datetime NOT NULL,
+  `is read` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
 -- Structure de la table `tokens`
 --
 
@@ -61,13 +77,23 @@ CREATE TABLE `users` (
   `firstname` varchar(60) NOT NULL,
   `lastname` varchar(60) NOT NULL,
   `username` varchar(60) NOT NULL,
-  `mail` varchar(60) NOT NULL,
+  `email` varchar(60) NOT NULL,
   `password` text NOT NULL,
   `country` varchar(60) NOT NULL,
   `city` varchar(60) NOT NULL,
   `zipcode` varchar(10) NOT NULL,
   `role` varchar(32) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Contenu de la table `users`
+--
+
+INSERT INTO `users` (`id`, `firstname`, `lastname`, `username`, `email`, `password`, `country`, `city`, `zipcode`, `role`) VALUES
+(1, '', '', '0rvn', 'toto@pourri.fr', '$2y$10$FZ.icSfx6M4Vg6SuonhG2eR7b5wu6Z9OllM6.1fbb5pS/D8U362.i', 'FR', 'Lille', '59000', ''),
+(2, 'Delphine', 'Titi', 'toto', 'blabla@pourri.fr', '$2y$10$xUHt6PHjkcuVjjGNOsqy.eTXClPThQEQOPDvPdoRnluvL3v1cEZbu', 'BS', 'Lille', '59000', ''),
+(3, 'Marc', 'Blabla', 'Marc1', 'tata@pourri.fr', '$2y$10$301XjjwPW81mqPbG8UDXFua6LoC0QnofabT1YfxruzTFC2QSA0B/a', 'AX', 'Lille', '59000', ''),
+(4, 'Delphine', 'Martinet', '0rvn', 'd-martinet@hotmail.fr', '$2y$10$ms5NHRgvWjvLoLV.qCGo5Omn0a/34opdvug1eJRZ4vlk3iDimlBEu', 'FR', 'Lille', '59000', '');
 
 -- --------------------------------------------------------
 
@@ -89,6 +115,12 @@ CREATE TABLE `usersclothes` (
 -- Index pour la table `clothes`
 --
 ALTER TABLE `clothes`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `contact`
+--
+ALTER TABLE `contact`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -122,6 +154,11 @@ ALTER TABLE `usersclothes`
 ALTER TABLE `clothes`
   MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
 --
+-- AUTO_INCREMENT pour la table `contact`
+--
+ALTER TABLE `contact`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+--
 -- AUTO_INCREMENT pour la table `tokens`
 --
 ALTER TABLE `tokens`
@@ -130,7 +167,7 @@ ALTER TABLE `tokens`
 -- AUTO_INCREMENT pour la table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 --
 -- AUTO_INCREMENT pour la table `usersclothes`
 --
--- app/Views/users/index.php
+++ app/Views/users/index.php
@@ -44,9 +44,44 @@
 						</div>
 
 						<!-- Ma garde-robe -->
-						<div class="col-md-4 col-md-offset-4">
+						<div class="col-md-6 col-md-offset-2">
 							<div class="text-center section-title">
 								<a href="<?= $this->url('clothes_read') ?>"><h2>Ma garde-robe</h2></a>
+							</div>
+
+							<!-- Thumbnails -->
+							<div class="row">
+							  <div class="col-sm-6 col-md-4">
+							    <div class="thumbnail">
+							      <img src="https://photos6.spartoo.com/photos/365/3654062/3654062_350_A.jpg" class="img-responsive imgClothes" id="imgWardrobe" alt="vêtement">
+							      <div class="caption">
+							        <h3>Thumbnail label</h3>
+							        <p>...</p>
+							        <p><a href="#" class="btn btn-dark" role="button">Button</a> <a href="#" class="btn btn-default" role="button">Button</a></p>
+							      </div>
+							    </div>
+							  </div>
+							  <div class="col-sm-6 col-md-4">
+							    <div class="thumbnail">
+							      <img src="http://www.celio.com/medias/sys_master/productMedias/productMediasImport/h2e/hc5/9144818696222/product-media-import-1040935-3-weared.jpg?frz-v914" class="img-responsive imgClothes" id="imgWardrobe" alt="vêtement">
+							      <div class="caption">
+							        <h3>Thumbnail label</h3>
+							        <p>...</p>
+							        <p><a href="#" class="btn btn-dark" role="button">Button</a> <a href="#" class="btn btn-default" role="button">Button</a></p>
+							      </div>
+							    </div>
+							  </div>
+							
+							  <div class="col-sm-6 col-md-4">
+							    <div class="thumbnail">
+							      <img src="https://media.quiksilver.co.id/media/catalog/product/cache/image/1000x1000/9df78eab33525d08d6e5fb8d27136e95/e/q/eqyws03252_quiksilver_mens_everyday_chino_walkshort_tmp0_1_h.jpg" class="img-responsive imgClothes" id="imgWardrobe" alt="vêtement">
+							      <div class="caption">
+							        <h3>Thumbnail label</h3>
+							        <p>...</p>
+							        <p><a href="#" class="btn btn-dark" role="button">Button</a> <a href="#" class="btn btn-default" role="button">Button</a></p>
+							      </div>
+							    </div>
+							  </div>
 							</div>
 							<div class="text-center">
 								<button type="submit" class="btn btn-dark">Créer ma garde-robe</button>
